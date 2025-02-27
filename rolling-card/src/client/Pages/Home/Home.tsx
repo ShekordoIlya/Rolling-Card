c7c56223c47305ac4f35eda9068851f3ee8807ec
Ilya Shekordo <27phantom03@list.ru>
2025-02-27 23:58:42 +0300

    feat(main-page):RTK slice was added

--- rolling-card/src/client/Pages/Home/Home.tsx
+++ rolling-card/src/client/Pages/Home/Home.tsx
@@ -1,7 +1,15 @@
+import { useDispatch, useSelector } from "react-redux";
 import { mainPageInfo } from "../../../Data/data.js";
+import { useEffect } from "react";
+import { setData } from "../../Redux/mainPageInfoSlice.js";
 
 const Home = () => {
-  return <div>{mainPageInfo[0].mainDescriptionFirst}</div>;
+  const { data } = useSelector((state) => state.mainPageStore);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setData(mainPageInfo[0].mainHeading));
+  }, []);
+  return <div>{data}</div>;
 };
 
 export default Home;
